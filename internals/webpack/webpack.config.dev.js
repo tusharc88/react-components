7a5e22e7d056dbd3da5b07b9c10e45edf1ce0e8e
tusharc88 <tushar54983@gmail.com>
2018-04-15 22:38:47 -0700

    setting up project

---
+++ internals/webpack/webpack.config.dev.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, '../../src/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, '../../dist'),
+    filename: '[name].bundle.js'
+  },
+  devtool: 'eval',
+  devServer: {
+    contentBase: path.resolve(__dirname, '../../dist'),
+    host: '127.0.0.1',
+    port: 3000
+    // compress: true,
+    // proxy
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    // new CleanWebpackPlugin(['dist'], {root: path.resolve(__dirname, '../../')}),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../../public/index.html')
+    })
+  ]
+}
